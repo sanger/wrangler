efb8c66153c2663694d67db62fadbbf48bae464e
Katy Taylor <kt17@mib113793i.internal.sanger.ac.uk>
2019-10-29 11:39:06 +0000

    delete random stuff added while learning/debugging

--- test_basic.py
+++ test_basic.py
@@ -1,19 +1,10 @@
 import pytest
 from flask import g, session
 from app import app
-import sys
-
-def func(x):
-	return x + 1
-
-def test_answer():
-	assert func(4) == 5
-
-def test_stuff():
-	print('HI')
-
 
 def test_get():
 	app.testing = True
 	client = app.test_client()
 	assert client.get('/rack/1234').status_code == 200
+
+def test_
