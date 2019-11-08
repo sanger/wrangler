46f46a82cacd0f8014820dc51e49dca99411ea1b
Katy Taylor <kt17@sanger.ac.uk>
2019-11-08 09:59:03 +0000

    Change the format of the response JSON structure - no need for it to be a list of objects, a single object is fine - switch it to put the tube barcode as the key, and the coordinate as the value, because that works better for using it in sequencescape

--- app.py
+++ app.py
@@ -17,10 +17,9 @@ def parse_tube_rack_csv(tube_rack_barcode: str) -> str:
     if isfile(full_path_to_find) and getsize(full_path_to_find) > 0:
         with open(full_path_to_find) as tube_rack_file:
             tube_rack_csv = csv.reader(tube_rack_file, delimiter=',')
-            layout = []
+            layout = {}
             for row in tube_rack_csv:
-                postition_dict = {row[0].strip(): row[1].strip()}
-                layout.append(postition_dict)
+                layout[row[1].strip()] = row[0].strip()
         return {'rack_barcode': tube_rack_barcode, 'layout': layout}
     else:
         return {'error': f'File ({full_path_to_find}) not found'}, 404
