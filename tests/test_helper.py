3b4ec3d672df320da43c4af963c9c1d41c363b0e
Katy Taylor <kt17@sanger.ac.uk>
2020-03-27 11:49:16 +0000

    add test cases for 96 and 48, to check wrangle_tubes output is as expected

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -36,6 +36,7 @@ def test_wrangle_tubes(app, client):
             "attributes": {
                 "tube_rack": {
                     "barcode": "DN123",
+                    "size": 96,
                     "tubes": [
                         {
                             "coordinate": "A01",
@@ -80,6 +81,17 @@ def test_wrangle_tubes(app, client):
         with raises(BarcodeNotFoundError):
             wrangle_tubes("")
 
+def test_wrangle_tubes_size_48(app, client):
+    with app.app_context():
+        tube_request_body = wrangle_tubes("DN_size48")
+
+        print("DEBUGGING HERE!")
+        print(tube_request_body)
+
+        assert tube_request_body['data']['attributes']['tube_rack']['size'] == 48
+
+        with raises(BarcodeNotFoundError):
+            wrangle_tubes("")
 
 def test_validate_tubes_different_barcodes():
     with raises(BarcodesMismatchError):
