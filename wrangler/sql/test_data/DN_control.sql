a1a30cd7d5ef10cc25134c4b704cbcde64858e36
Chris Smith <cs24@sanger.ac.uk>
2020-06-01 18:02:03 +0100

    Add missing values to some SQL test data.

--- wrangler/sql/test_data/DN_control.sql
+++ wrangler/sql/test_data/DN_control.sql
@@ -1,4 +1,4 @@
-INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB133','PHEC-nnnnnnn60','A01');
-INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB134','Positive control','A02');
-INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB135','Negative control','A03');
-INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB136','Control','A04');
+INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB133','PHEC-nnnnnnn60','A01','heron', 'DNAP', null);
+INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB134','Positive control','A02','heron', 'DNAP', null);
+INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB135','Negative control','A03','heron', 'DNAP', null);
+INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB136','Control','A04','heron', 'DNAP', null);
