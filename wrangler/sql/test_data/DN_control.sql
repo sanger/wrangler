2989bc089776e212880f4013b3e095a96c386a8d
Katy Taylor <kt17@sanger.ac.uk>
2020-06-03 10:29:45 +0100

    add sample_type column

--- wrangler/sql/test_data/DN_control.sql
+++ wrangler/sql/test_data/DN_control.sql
@@ -1,4 +1,4 @@
-INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB133','PHEC-nnnnnnn60','A01','heron', 'DNAP', null);
-INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB134','Positive control','A02','heron', 'DNAP', null);
-INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB135','Negative control','A03','heron', 'DNAP', null);
-INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB136','Control','A04','heron', 'DNAP', null);
+INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB133','PHEC-nnnnnnn60','A01', 'extract', 'heron', 'DNAP', null);
+INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB134','Positive control','A02', 'extract', 'heron', 'DNAP', null);
+INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB135','Negative control','A03', 'extract', 'heron', 'DNAP', null);
+INSERT INTO `{{ database }}`.`cgap_heron` VALUES (null,'DN_control','TB136','Control','A04', 'extract', 'heron', 'DNAP', null);
